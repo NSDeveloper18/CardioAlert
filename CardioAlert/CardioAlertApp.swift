592435cbe298c22a704d7105f1f87a2cbed0de63
Shakhzod <sbotirov600@gmail.com>
2024-09-21 09:23:46 +0500

    Initial Commit

---
+++ CardioAlert/CardioAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardioAlertApp.swift
+//  CardioAlert
+//
+//  Created by Shakhzod Botirov on 21/09/24.
+//
+
+import SwiftUI
+
+@main
+struct CardioAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
